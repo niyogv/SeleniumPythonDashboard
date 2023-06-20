0c6fa89bdaafff91602ba93162363e311ea683bd
Niyog V <77136963+niyogv@users.noreply.github.com>
2023-06-20 15:11:48 +0530

    Updated test_digitalme.py

--- test_digitalme.py
+++ test_digitalme.py
@@ -1,4 +1,6 @@
 import time
+import random
+import string
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,30 +10,12 @@ from selenium.webdriver.common.keys import Keys
 username=''
 password=''
 
-# edit profile
-fname='niyog'
-lname='v'
-mail='niyog@google.com'
-number='9999999999'
-
-# update profile
-firstname='nihal'
-lastname='v'
-e='niyog@yahoo.co.in'
-phone='8888888888'
-
-# bio
-bio='tester'
-
-# update bio
-updatebio='qa'
-
 class Test_dashboard:
 
-    @pytest.fixture()  #Predefined step which run everytime before executing particular test case
+    @pytest.fixture()
     def test_invoke(self):
         self.driver=webdriver.Chrome()
-        self.driver.get('https:www.example.com')
+        self.driver.get('https://qadashboard.iome.ai')
         self.driver.maximize_window()
         time.sleep(1)
         self.driver.find_element(By.XPATH, '//button').click()
@@ -40,7 +24,7 @@ class Test_dashboard:
         self.driver.find_element(By.XPATH, '//button[@type="submit"]').click()
         time.sleep(6)
         check=self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/section/section/div/div[1]')
-        assert check.text=='Hello!', 'Login failed' #Checks for validation
+        assert check.text=='Hello!', 'Login failed'
 
     def test_kyc(self,test_invoke):
         self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/section/section/section/main/div/div[1]/div[2]/button[1]').click()
@@ -58,10 +42,14 @@ class Test_dashboard:
         time.sleep(6)
 
     def test_edit_profile(self,test_invoke):
+        fname=''.join(random.choices(string.ascii_lowercase,k=4))  #Generates the random first name with lenght 4
+        lname=''.join(random.choices(string.ascii_letters,k=1))  #Generates the random last name with lenght 1
+        number=''.join(random.choices(string.digits,k=10))  #Generates the random number with lenght 10
+        mail=fname+'@'+lname+'.com'  #Generates the mail id using the fname and lname
         self.driver.find_element(By.XPATH, '//button[2]').click()
         time.sleep(2)
         check1=self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/button/span')
-        assert check1.text=='Save information', 'Edit profile page is broken'  #Checks for validation
+        assert check1.text=='Save information', 'Edit profile page is broken'
         self.driver.find_element(By.XPATH, '//input[@placeholder="What is your first name?"]').send_keys(fname)
         self.driver.find_element(By.XPATH, '//input[@placeholder="What is your last name?"]').send_keys(lname)
         self.driver.find_element(By.XPATH, '//input[@placeholder="unique@rmail.com"]').send_keys(mail)
@@ -72,10 +60,14 @@ class Test_dashboard:
         time.sleep(4)
 
     def test_update_profile(self,test_invoke):
+        firstname=''.join(random.choices(string.ascii_letters,k=4))
+        lastname=''.join(random.choices(string.ascii_lowercase,k=1))
+        e=firstname+'@'+lastname+'.in'
+        phone=''.join(random.choices(string.digits,k=10))
         self.driver.find_element(By.XPATH, '//button[2]').click()
         time.sleep(2)
         check2=self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/button/span')
-        assert check2.text=='Save information', 'Update page is broken'  #Checks for validation
+        assert check2.text=='Save information', 'Update page is broken'
         first=self.driver.find_element(By.XPATH, '//input[@placeholder="What is your first name?"]')
         for f in range(5):
             first.send_keys(Keys.BACKSPACE)
@@ -93,25 +85,27 @@ class Test_dashboard:
         self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/button').click()
         time.sleep(4)
 
-    @pytest.mark.flaky(rerun=2)  #This particular test run twice if it fails just to make sure that it is working
+    @pytest.mark.flaky(rerun=2)
     def test_bio(self,test_invoke):
+        bio=''.join(random.choices(string.ascii_lowercase,k=4))  #Generates the random character with lenght 4
         time.sleep(2)
         self.driver.find_element(By.XPATH, '//textarea').send_keys(bio)
         self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/section/section/section/main/div/div[2]/div[2]/div/div/div/div/div[3]/div[2]/button').click()
         time.sleep(2)
 
-    @pytest.mark.flaky(rerun=2)  #This particular test run twice if it fails just to make sure that it is working
+    @pytest.mark.flaky(rerun=2)
     def test_update_bio(self,test_invoke):
+        updatebio=''.join(random.choices(string.ascii_letters,k=6))
         time.sleep(2)
         self.driver.find_element(By.XPATH, '//img[@alt="edit icon"]').click()
         b=self.driver.find_element(By.XPATH, '//textarea')
-        for y in range(6):
+        for y in range(4):
             b.send_keys(Keys.BACKSPACE)
         self.driver.find_element(By.XPATH, '//textarea').send_keys(updatebio)
         self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/section/section/section/main/div/div[2]/div[2]/div/div/div/div/div[3]/div[2]/button').click()
         time.sleep(2)
 
-    @pytest.mark.flaky(rerun=2)  #This particular test run twice if it fails just to make sure that it is working
+    @pytest.mark.flaky(rerun=2)
     def test_social(self,test_invoke):
         time.sleep(2)
         twit=self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/section/section/section/main/div/div[2]/div[1]/div[2]/div/div/div[2]/div[1]/div[2]/a')
@@ -123,5 +117,7 @@ class Test_dashboard:
         discord=self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/section/section/section/main/div/div[2]/div[1]/div[2]/div/div/div[2]/div[2]/div[2]/a')
         discord.click()
         time.sleep(4)
+
+
 
 
