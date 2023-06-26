82dd7213aa127945915cfc3411d375ff13e7f217
Niyog V <77136963+niyogv@users.noreply.github.com>
2023-06-26 13:32:09 +0530

    Update test_digitalme.py

--- test_digitalme.py
+++ test_digitalme.py
@@ -14,8 +14,8 @@ chrome_options=Options()
 chrome_options.add_argument('--headless')
 
 # login cred
-username = os.environ('USERNAME')
-password = os.environ('PASSWORD')
+username = os.environ.get('USERNAME')
+password = os.environ.get('PASSWORD')
 
 class Test_dashboard:
 
